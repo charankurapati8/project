ba6e368ce1349066585ff9fa16197f0f1759cf5c
charan <charanreddykurapati08@gmail.com>
2025-02-08 19:10:43 +0530

    routing created b/w pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Landingpage from './stores/pages/landingpage'
+import {Route, Routes} from 'react-router-dom'
+import Mobilepage from './stores/pages/mobilepage'
+import Computerpage from './stores/pages/computerpage'
+import Tvpage from './stores/pages/tvpage'
+import Acpage from './stores/pages/acpage'
+import Watchpage from './stores/pages/watchpage'
 const App = () => {
   return (
     <div>
-     <Landingpage/>
+      <Routes>
+        <Route path ='/' element={ <Landingpage/>}/>
+        <Route path='mobile' element = {<Mobilepage/>}/>
+        <Route path='computer' element = {<Computerpage/>}/>
+        <Route path='tv' element = {<Tvpage/>}/>
+        <Route path='ac' element = {<Acpage/>}/>
+        <Route path='watches' element = {<Watchpage/>}/>
+      </Routes>
+     
     </div>
   )
 }
--- src/stores/components/navbar.js
+++ src/stores/components/navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
+    <>
     <div className='navsection'> 
       <div className='title'>
         E-CART
@@ -14,6 +16,16 @@ const Navbar = () => {
         CART
       </div>
     </div>
+    <div className='submenu'>
+      <ul>
+        <Link to='/mobile'> <li>Mobiles</li></Link>
+        <Link to='/computer'><li>Computers</li></Link>
+        <Link to='/tv'><li>TV</li></Link>
+        <Link to='/ac'><li>AC</li></Link>
+        <Link to='/watches'><li>Watches</li></Link>
+      </ul>
+    </div>
+    </>
   )
 }
 
